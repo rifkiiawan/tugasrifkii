0202a69b10222d6bab3deea85b7d7bd081ea26a5
rifkiiawan <rifkisetiawandark@gmail.com>
2021-07-31 07:11:41 +0700

    first commit

---
+++ sp2 tugas5nomor2/nomor2.js
@@ -0,0 +1,10 @@
+for (i = 1; i<=20; i++)
+    if(i % 2 != 0 && i %3 == 0){
+        document.write(i +'_'+'I love coding' + '<br/>')
+    }
+    else if (i % 2 != 0){
+        document.write(i + '_' + 'santai' + '<br/>')
+    }
+    else if (i%2 ==0){
+        document.write(i + '_' + 'berkualitas' + '<br>')
+    }
---
+++ sp2tugas5nomor1/sp2tgs5_nmr1.js
@@ -0,0 +1,4 @@
+function hasilkali(num1, num2){            
+    return num1*num2;
+}            
+console.log(hasilkali(12,4)); 
